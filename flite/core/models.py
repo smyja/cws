e2c7f81c36ecf37e91d63d52da16b0dcfd64e199
Smyja <akposlive59@gmail.com>
2024-05-02 13:20:33 +0100

    update

--- flite/core/models.py
+++ flite/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.core.validators import MinValueValidator
 
 class BaseModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -11,23 +12,21 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
-
-class BudgetCategory(models.Model):
+class BudgetCategory(BaseModel):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='budget_categories')
     name = models.CharField(max_length=200)
     description = models.TextField()
-    max_spend = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    max_spend = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
+
     def __str__(self):
         return self.name
 
-class Transaction(models.Model):
-    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name='transactions')
-    # other fields...
+class Transaction(BaseModel):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='transactions')
     category = models.ForeignKey(BudgetCategory, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    
+
     def __str__(self):
         return f"{self.category.name} - {self.amount}"
